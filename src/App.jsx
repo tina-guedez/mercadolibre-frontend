67d1123a72205459cffa3110aa75b9407b4cdbf9
Valentina Guedez <guedezvalentina@gmail.com>
2022-08-31 09:45:06 -0300

    Added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import SearchResult from './pages/SearchResult/components/SearchResult';
-import ItemDetail from './pages/ItemDetail/ItemDetail';
 import './App.css';
+
+// Importaciones dinámicas para uso de lazy. Permite cargar un componente solo cuando es llamado.
+const SearchResult = React.lazy(() => { return import('./pages/SearchResult/components/SearchResult'); });
+const ItemDetail = React.lazy(() => { return import('./pages/ItemDetail/ItemDetail'); });
+const Home = React.lazy(() => { return import('./pages/Home/Home'); });
 
 function App() {
   return (
-    <div className="container-main">
-      <Header />
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/items" element={<SearchResult />} />
-          <Route path="/items/:id" element={<ItemDetail />} />
-        </Routes>
-      </Router>
-    </div>
+    <Suspense fallback={<>...</>}>
+      <div className="container-main">
+        <Header />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/items" element={<SearchResult />} />
+            <Route path="/items/:id" element={<ItemDetail />} />
+          </Routes>
+        </Router>
+      </div>
+    </Suspense>
   );
 }
 
