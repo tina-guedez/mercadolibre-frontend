d7b3aab633b598e5cd18e20f3eed2371e4c89122
Valentina Guedez <guedezvalentina@gmail.com>
2022-08-30 23:41:01 -0300

    Updated config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports={
-  entry:'./src/index.js',
-  output:{
-    path:path.resolve(__dirname, 'dist'),
-    filename:'bundle.js',
-    publicPath:'/'
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-  resolve:{
-    extensions:['.js','.jsx']
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
-  module:{
-    rules:[
-        {
-          test:/\.(js|jsx)$/,
-          exclude:/node_modules/,
-          use:{
-            loader:'babel-loader',
-          }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test:/\.html$/,
-          use:{
-            loader:'html-loader',
-          }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
         },
-        {
-          test:/\.(s*)css$/,
-          use:[
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-          ]
-        }
-    ]            
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+    ],
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html',
@@ -45,13 +49,13 @@ module.exports={
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
   ],
-  devServer:{
+  devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
     open: true,
     historyApiFallback: true,
-  }
-}
+  },
+};
