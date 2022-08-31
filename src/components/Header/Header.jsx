a873c3525726be48ff019e80a60ab467411a312d
Valentina Guedez <guedezvalentina@gmail.com>
2022-08-31 11:03:50 -0300

    Added Link to routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 
 export default function Header() {
   return (
     <header className="nav">
       <div className="nav-menu">
-        <a href="/" tabIndex="0" className="nav-logo" />
+        <Link to="/" className="nav-logo" />
         <SearchBar />
       </div>
     </header>
