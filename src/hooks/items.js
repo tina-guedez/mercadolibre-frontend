760f3f0d88a9df29bd6bbf1645a6d66068f1ae0c
Valentina Guedez <guedezvalentina@gmail.com>
2022-09-02 00:32:39 -0300

    Added validation for query empty

--- src/hooks/items.js
+++ src/hooks/items.js
@@ -1,6 +1,7 @@
 const URL_BASE = 'http://localhost:3000/api/items';
 
 export const useDataFetchItem = async (q) => {
+  if (q === undefined) return null;
   const req = await fetch(`${URL_BASE}?q=${encodeURI(q)}`);
   const res = await req.json();
   return req.status === 200 ? res : null;
